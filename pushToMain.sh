02a207c3e7d4e9141fe8ab3af2056af3fd1ed897
Lorenzo996 <32709654+Lorenzo996@users.noreply.github.com>
2024-10-02 10:41:07 +0200

    Updated pushToMain.sh

--- pushToMain.sh
+++ pushToMain.sh
@@ -1,21 +1,32 @@
-# Switch to local-dev Branch
-git checkout local-dev
+# Switch to dev-local Branch
+git checkout dev-local
 # Perform the Find-and-Replace Operation
-find . -type f -name "*.js" -exec sed -i 's/{apiURL}\/\({.*}\)/{apiURL}\1/g' {} +
+cd src/components
+find . -type f -name "*.js" -exec sed -i '' "s|{apiUrl}/|{apiUrl}|g" {} + 
+cd ../..
 git add .
 git commit -m "Replaced {apiURL}/... with {apiURL}..."
 # Switch to main Branch
 git checkout main
-# Merge local-dev Branch into main Branch (Excluding Specific Files)
-git merge local-dev --no-commit # use the --no-commit option to stage the changes first, then manually remove the unwanted files
+# Merge dev-local Branch into main Branch (Excluding Specific Files)
+# use the --no-commit option to stage the changes first, then manually remove the unwanted files
+git merge dev-local --no-commit 
 # Restore the Excluded Files
 git restore --source=main .env
-git restore --source=main /fantaheroes/settings.py
 git restore --source=main pushToMain.sh
 # Commit the Merge
-git commit -m "Merged local-dev into main, excluding .env and settings.py"
+git commit -m "Merged dev-local into main, excluding .env and pushToMain.sh"
 # Push the Changes to GitHub
 git push origin main
 # Rebuild and deploy the App
 npm run deploy
-# Switch back to local-dev Branch
+# Switch back to dev-local Branch
+git checkout dev-local
+# Perform the Find-and-Replace Operation
+cd src/components
+find . -type f -name "*.js" -exec sed -i '' "s|{apiUrl}|{apiUrl}/|g" {} +
+cd ../..
+git add .
+git commit -m "Replaced {apiURL} with {apiURL}/..."
+# Push the Changes to GitHub
+git push origin dev-local
